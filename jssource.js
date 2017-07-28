9c8878bf9c35dd4a2e293210a8ad5d3cf53d8024
SolarPolarMan <SolarPolarMan@gmail.com>
2017-07-28 18:41:16 +0100

    Should finally work?

--- jssource.js
+++ jssource.js
@@ -1,13 +1,9 @@
-
 var now = new Date(); //constant
 
 var ref = new Date(1501196400000); // A day that had games added on it
 var testDay = ref;
 
-while (testDay < now){
-  testDay = testDay.setDate(testDay.getDate() + 14)
-}
-testDay.setHours(16);
+
 
 function sOrNah(input, unit){
   if (input == 1){
@@ -17,40 +13,45 @@ function sOrNah(input, unit){
   }
 }
 
-
-window.onload = function() {
-
+function showRemaining() {
   var _second = 1000;
   var _minute = _second * 60;
   var _hour = _minute * 60;
   var _day = _hour * 24;
 
+  var now = new Date();
+  var distance = testDay - now;
+  if (distance < 0) {
 
-  function showRemaining() {
-    var now = new Date();
-    var distance = testDay - now;
-    if (distance < 0) {
+    clearInterval(timer);
+    document.getElementById("time").innerHTML = "EXPIRED!";
 
-      clearInterval(timer);
-      document.getElementById("time").innerHTML = "EXPIRED!";
-
-      return;
-    }
-
-    var days = Math.floor(distance / _day);
-    var hours = Math.floor((distance % _day) / _hour);
-    var minutes = Math.floor((distance % _hour) / _minute);
-    var seconds = Math.floor((distance % _minute) / _second);
-
-    document.getElementById("time").innerHTML = "";
-    sOrNah(days,"day");
-    sOrNah(hours,"hour");
-    sOrNah(minutes, "min");
-    sOrNah(seconds, "second");
+    return;
   }
 
-  var timer = window.setInterval(function(){showRemaining()}, 1000);//Update timer once a second
+  var days = Math.floor(distance / _day);
+  var hours = Math.floor((distance % _day) / _hour);
+  var minutes = Math.floor((distance % _hour) / _minute);
+  var seconds = Math.floor((distance % _minute) / _second);
 
+  document.getElementById("time").innerHTML = "";
+  sOrNah(days,"day");
+  sOrNah(hours,"hour");
+  sOrNah(minutes, "min");
+  sOrNah(seconds, "second");
+}
+
+window.onload = function() {
+
+  while (testDay < now){
+    testDay = new Date(testDay.setDate(testDay.getDate() + 14)) //add 2 weeks
+    console.log(testDay);
+  }
+
+  testDay.setHours(16);
+
+
+  var timer = window.setInterval(function(){showRemaining()}, 1000);//Update timer once a second
 
   showRemaining()
 
