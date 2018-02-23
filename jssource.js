d56d6994a273f3f73cd613054db1684b0908c19a
ChildishGiant <ChildishGiant@gmail.com>
2018-02-23 23:10:15 +0000

    The KSib update

--- jssource.js
+++ jssource.js
@@ -1,55 +1,51 @@
-var now = new Date(); //constant
+var rollingChronoShopDate;
 
-var ref = new Date("Fri Sept 22 2017 17:00:00 GMT+0100 (GMT Daylight Time)"); // A day that had games added on it
-var testDay = ref;
-
-
-
-function sOrNah(input, unit){
-  if (input == 1){
-    document.getElementById("dusty").innerHTML += "1 "+unit+" ";
-  } else {
-    document.getElementById("dusty").innerHTML += input + " "+unit+"s ";
-  }
+function toggleImage(willShow) {
+    if (willShow) {
+        document.getElementById("quoteDiv").style.display = "block";
+    } else {
+        document.getElementById("quoteDiv").style.display = "none";
+    }
 }
 
-function showRemaining() {
-  var _second = 1000;
-  var _minute = _second * 60;
-  var _hour = _minute * 60;
-  var _day = _hour * 24;
+function refreshData() {
+    var currentUtcDateTime = moment.utc();
+    rollingChronoShopDate = moment.utc("2017-03-10T17:00:00Z");
+    while (currentUtcDateTime.isAfter(rollingChronoShopDate)) {
+        rollingChronoShopDate.add(14, "days");
+    }
 
-  var now = new Date().getTime();
-  var distance = testDay - now;
-  if (distance < 0) {
+    var formattedNextCoinShopDate = rollingChronoShopDate.local().format("MMMM Do") + " at " +
+                                    rollingChronoShopDate.format("hA");
 
-    clearInterval(timer);
-    document.getElementById("dusty").innerHTML = "EXPIRED!";
-
-    return;
-  }
-
-  var days = Math.floor(distance / _day);
-  var hours = Math.floor((distance % _day) / _hour);
-  var minutes = Math.floor((distance % _hour) / _minute);
-  var seconds = Math.floor((distance % _minute) / _second);
-
-  document.getElementById("dusty").innerHTML = "";
-  sOrNah(days,"day");
-  sOrNah(hours,"hour");
-  sOrNah(minutes, "min");
-  sOrNah(seconds, "second");
+    document.getElementById("cameojokes").innerText = "That's " + formattedNextCoinShopDate + " your time, buddy.";
 }
 
-window.onload = function() {
+function getTimeRemaining() {
+    var utcNow = moment.utc();
 
-  while (testDay < now){
-    testDay = new Date(testDay.setDate(testDay.getDate() + 14)) //add 2 weeks
-  }
+    if (utcNow.isAfter(rollingChronoShopDate)) {
+        refreshData();
+    }
 
-  var timer = window.setInterval(function(){showRemaining()}, 1000);//Update timer once a second
+    var rollingChronoShopDateInner = moment.utc(rollingChronoShopDate);
+	var diff = rollingChronoShopDateInner.diff(utcNow);
+	var duration = moment.duration(diff);
 
-  showRemaining()
+    return duration.days() + " days, " +
+           duration.hours() + " hours, " +
+           duration.minutes() + " minutes, " +
+           duration.seconds() + " seconds";
+}
 
+function updateTimer() {
+    var timeRemaining = getTimeRemaining();
+    document.getElementById("dusty").innerText = timeRemaining;
+}
 
-};
+// call once so screen isn't empty
+refreshData();
+updateTimer();
+
+// Set up timer to refresh every second for restock date
+setInterval(updateTimer, 1000);
